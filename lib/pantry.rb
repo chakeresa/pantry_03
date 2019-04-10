f93d02b0a52f2bebfc3f65e87d99b0b85bb983fe
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-10 10:44:24 -0600

    Add Pantry and its init

---
+++ lib/pantry.rb
@@ -0,0 +1,10 @@
+require './lib/ingredient'
+require './lib/recipe'
+
+class Pantry
+  attr_reader :stock
+  
+  def initialize
+    @stock = {}
+  end
+end
---
+++ test/pantry_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+    @cheese = Ingredient.new("Cheese", "C", 50)
+    @mac = Ingredient.new("Macaroni", "oz", 200)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_stock_inits_to_an_empty_hash
+    assert_equal ({}), @pantry.stock
+  end
+end
